2aaa46f75e5b8f4c71c4ee1f5eb6256b6b1c77ce
Dmitry Belousov <belavusdm@gmail.com>
2024-07-07 21:35:36 +0300

    Add comments

--- src/Ozon.MerchService.Domain/DataContracts/IUnitOfWork.cs
+++ src/Ozon.MerchService.Domain/DataContracts/IUnitOfWork.cs
@@ -2,11 +2,27 @@ using Npgsql;
 
 namespace Ozon.MerchService.Domain.DataContracts;
 
+/// <summary>
+/// Unit of work interface
+/// </summary>
 public interface IUnitOfWork
 {
+    /// <summary>
+    /// Start transaction
+    /// </summary>
+    /// <param name="token">Cancellation token</param>
+    /// <returns></returns>
     ValueTask StartTransaction(CancellationToken token);
     
+    /// <summary>
+    /// Npgsql connection, that created after transaction creation
+    /// </summary>
     NpgsqlConnection Connection { get; }
-        
+    
+    /// <summary>
+    /// Save all changes in transaction to database
+    /// </summary>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns></returns>
     Task SaveChangesAsync(CancellationToken cancellationToken);
 }
